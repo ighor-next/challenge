d714915ac04cb8e5fb8da851f12fef58e8f0457c
FlokiTV <dawysonw@gmail.com>
2025-01-31 08:49:44 -0300

    feat: add base panel structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 function App() {
-
-  return <div className="fixed inset-0 flex items-center justify-center">
-    <div className="border rounded">
-      <div className="text-">Tarefas</div>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-zinc-50">
+      <div className="max-w-[1200px] min-h-[100vh] md:min-h-[80vh] w-full p-6 px-8 bg-white rounded-xl shadow">
+        <div className="text-3xl font-bold uppercase">Tarefas</div>
+        <div className="flex gap-2 mt-4">
+          <div className="flex-1 border rounded p-1 px-2">Pendente</div>
+          <div className="flex-1 border rounded p-1 px-2">Em andamento</div>
+          <div className="flex-1 border rounded p-1 px-2">Feito</div>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
