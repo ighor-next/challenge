f888c39297f3bdadae05200a873dac4a4f84c1dd
FlokiTV <dawysonw@gmail.com>
2025-01-31 16:08:30 -0300

    feat: new tasks resource

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './api/tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
