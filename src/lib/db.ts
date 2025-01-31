c6c9ce22dcd478bc902a608a14f6b069efd76897
FlokiTV <dawysonw@gmail.com>
2025-01-31 09:14:16 -0300

    feat: simple in-memory db

---
+++ src/lib/db.ts
@@ -0,0 +1,51 @@
+const TasksEnum = {
+  PENDING: "Pendente",
+  PROGRESS: "Em andamento",
+  COMPLETED: "Feito",
+} as const;
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  status: keyof typeof TasksEnum;
+}
+
+const Tasks: Task[] = [];
+
+export function getKeys() {
+  return TasksEnum;
+}
+
+export function getAll() {
+  return Tasks;
+}
+
+export function add(task: Task) {
+  Tasks.push(task);
+  return task;
+}
+
+export function update(task: Task) {
+  const index = Tasks.findIndex((t) => t.id === task.id);
+  if (index !== -1) {
+    Tasks[index] = task;
+  }
+  return task;
+}
+
+export function move(id: number, status: keyof typeof TasksEnum) {
+  const task = Tasks.find((t) => t.id === id);
+  if (task) {
+    task.status = status;
+  }
+  return task;
+}
+
+export function remove(id: number) {
+  const index = Tasks.findIndex((t) => t.id === id);
+  if (index !== -1) {
+    Tasks.splice(index, 1);
+  }
+  return id;
+}
