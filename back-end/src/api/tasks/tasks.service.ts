948c3f2198d59d89e341761d88258d4304390b2f
FlokiTV <dawysonw@gmail.com>
2025-01-31 16:31:38 -0300

    feat: new in-memory database

--- back-end/src/api/tasks/tasks.service.ts
+++ back-end/src/api/tasks/tasks.service.ts
@@ -2,25 +2,62 @@ import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
+const TasksEnum = {
+  PENDING: 'Pendente',
+  PROGRESS: 'Em andamento',
+  COMPLETED: 'Feito',
+} as const;
+
+export type TaskStatus = {
+  [key: string]: string;
+};
+
+export interface Task {
+  id?: number;
+  title: string;
+  description: string;
+  status: keyof TaskStatus;
+}
+
+const Tasks: Task[] = [];
+
 @Injectable()
 export class TasksService {
   create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+    const newTask = {
+      id: Date.now(),
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      status: TasksEnum.PENDING,
+    };
+
+    Tasks.push(newTask);
+    return newTask;
   }
 
   findAll() {
-    return `This action returns all tasks`;
+    return Tasks;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} task`;
+    return Tasks.find((task) => task.id === id);
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+    const task = this.findOne(id);
+    if (!task) throw new Error('Task not found');
+    task.title = updateTaskDto.title || task.title;
+    task.description = updateTaskDto.description || task.description;
+    task.status = updateTaskDto.status || task.status;
+
+    return task;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} task`;
+    const task = this.findOne(id);
+    if (!task) throw new Error('Task not found');
+    Tasks.splice(Tasks.indexOf(task), 1);
+
+    return { ok: true };
   }
 }
