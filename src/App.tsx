8344ff9f64fa1e8e0560e609baa71f47c6bd1c07
FlokiTV <dawysonw@gmail.com>
2025-01-31 09:52:42 -0300

    feat: add simple form to tests

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,13 @@ function App() {
     setTasks([...db.getAll()]);
   }
 
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const values = Object.fromEntries(formData.entries());
+    addTask(String(values.title), String(values.description));
+  }
+
   useEffect(() => {
     setKeys(db.getKeys());
     setTasks([...db.getAll()]);
@@ -22,13 +29,27 @@ function App() {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-zinc-50">
-      <div className="max-w-[1200px] min-h-[100vh] md:min-h-[80vh] w-full p-6 px-8 bg-white rounded-xl shadow">
+      <div className="max-w-[1200px] flex flex-col min-h-[100vh] md:min-h-[80vh] w-full p-6 px-8 bg-white rounded-xl shadow">
         <div className="text-3xl font-bold uppercase">Tarefas</div>
-        <div className="flex gap-2 mt-4">
+        <form onSubmit={onSubmit}>
+          <label>
+            <span>Título:</span>
+            <input type="text" name="title" required />
+          </label>
+          <label>
+            <span>Descrição:</span>
+            <textarea name="description" required></textarea>
+          </label>
+          <button>Adicionar</button>
+        </form>
+        <div className="flex grow gap-2 mt-4">
           {Object.keys(keys)?.map((key, index) => (
-            <div key={index} className="flex-1 border rounded p-1 px-2">
-              <div>{keys[key]}</div>
-              <div>
+            <div
+              key={index}
+              className="flex-1 border rounded p-1 px-2 overflow-auto"
+            >
+              <div className="text-lg font-semibold">{keys[key]}</div>
+              <div className="flex flex-col gap-1 mt-2">
                 {tasks
                   .filter((task) => task.status === key)
                   .map((task, index) => (
