74015e97cdc8c01f3f406c4469e665fdb67fd397
FlokiTV <dawysonw@gmail.com>
2025-01-31 16:31:12 -0300

    build: add swagger

--- back-end/src/api/tasks/dto/create-task.dto.ts
+++ back-end/src/api/tasks/dto/create-task.dto.ts
@@ -1,1 +1,10 @@
-export class CreateTaskDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTaskDto {
+  @ApiProperty({ description: 'Título do tarefa', example: 'Beber água' })
+  title: string;
+  @ApiProperty({ description: 'Descrição da tarefa', example: 'Fazer café' })
+  description: string;
+  @ApiProperty({ description: 'Status da tarefa', example: 'PENDING' })
+  status: string;
+}
