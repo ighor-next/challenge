ebb02ccdd5ef9c6f8771e33156bd2e39ff5869ce
FlokiTV <dawysonw@gmail.com>
2025-01-31 11:46:22 -0300

    feat: new card form

---
+++ src/components/cardForm.tsx
@@ -0,0 +1,47 @@
+interface ConfirmDeleteProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  onCancel: () => void;
+}
+
+export default function CardForm({ onSubmit, onCancel }: ConfirmDeleteProps) {
+  return (
+    <div className="fixed z-20 inset-0 flex items-center justify-center bg-black/20 px-10">
+      <div className="flex flex-col w-full p-4 bg-white rounded-xl shadow max-w-sm">
+        <div className="text-xl text-center font-bold uppercase">
+          ✅ Adicionar tarefa
+        </div>
+        <form onSubmit={onSubmit} className="flex flex-col mt-4">
+          <label className="flex flex-col gap-1">
+            <span>Título:</span>
+            <input
+              className="border border-zinc-400 rounded outline-none p-1 px-2"
+              type="text"
+              name="title"
+              required
+            />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span>Descrição:</span>
+            <textarea
+              name="description"
+              className="border border-zinc-400 rounded outline-none p-1 px-2"
+              required
+            ></textarea>
+          </label>
+          <button
+            type="submit"
+            className="mt-4 p-2 bg-green-500 text-white rounded hover:bg-green-600 cursor-pointer"
+          >
+            Adicionar
+          </button>
+        </form>
+        <button
+          onClick={onCancel}
+          className="mt-2 p-2 bg-gray-500 text-white rounded hover:bg-gray-600 cursor-pointer"
+        >
+          Cancelar
+        </button>
+      </div>
+    </div>
+  );
+}
