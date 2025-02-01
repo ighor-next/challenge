b91a27304805d1300c55961b66c25a77cd04eda8
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-01 19:36:54 -0300

    style: add media size

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xm': '480px',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
